2aab843878fde411820f678e3382e0470d2015d6
jayeshyadav24 <jayeshyadav24gmail.com>
2025-06-23 00:06:15 +0530

    step 5 admin routes

---
+++ routes/admin.js
@@ -0,0 +1,39 @@
+const {Router} = require('express');
+const adminRouter = Router();
+
+adminRouter.post("ad",function(req,res){
+        adminRouter.post("/signup",function(req,res){
+            res.json({
+                message: "signup admin endPoint"
+            })
+        })
+
+        adminRouter.post("/signin",function(req,res){
+            res.json({
+                message: "signin admin endPoint"
+            })
+        })
+
+        adminRouter.post("/course",function(req,res){
+            res.json({
+                message: "course admin endPoint"
+            })
+        })
+
+        adminRouter.put("/course",function(req,res){
+            res.json({
+                message: "course admin endPoint"
+            })
+        })
+
+        adminRouter.get("/course/bulk",function(req,res){
+            res.json({
+                message: "course admin endPoint"
+            })
+        })
+    }
+)
+
+module.exports ={
+    adminRouter: adminRouter    
+}
